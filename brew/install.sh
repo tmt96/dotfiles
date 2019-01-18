749d1284c37ffd0a42fda474a3445b3011efa654
tmt <minhtuan.tran96@gmail.com>
2019-01-18 00:19:15 -0800

    tweaks for brew & emacs layers

--- brew/install.sh
+++ brew/install.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+echo
+echo "Syncing apps through Homebrew..."
 if ! [ -x "$(command -v brew)" ]; then
-    echo "Installing homebrew..."
+    echo "You don't have Homebrew installed. Installing homebrew..."
     xcode-select install
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi;
 
-echo "Installing your apps..."
-echo "$(dirname ${BASH_SOURCE[0]})/Brewfile"
+echo "Installing your apps using Brewfile..."
 brew bundle --file="$(dirname ${BASH_SOURCE[0]})/Brewfile"
 
 echo "Linking your shell config files..."
-ln -sfn "$(dirname ${BASH_SOURCE[0]})/Brewfile" ~/Brewfile
+dir_name=`dirname "${BASH_SOURCE[0]}"`
+dir_name=`( cd "$dir_name" && pwd )`
+ln -sfn "$dir_name/Brewfile" ~/Brewfile
+
+echo "Done installing apps from Homebrew!!"
+echo
--- emacs/install.sh
+++ emacs/install.sh
@@ -1,12 +1,17 @@
-echo "CONFIURING EMACS..."
+
+echo
+echo "Confiuring emacs..."
 if [ ! -d ~/.emacs.d ]; then
     echo "You don't have an emacs config. Cloning spacemacs..."
     git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
 echo "Symlinking spacemacs config..."
-ln -sfn "$(dirname ${BASH_SOURCE[0]})/.spacemacs" ~/.spacemacs
+dir_name=`dirname "${BASH_SOURCE[0]}"`
+dir_name=`( cd "$dir_name" && pwd )`
+ln -sfn "$dir_name/.spacemacs" ~/.spacemacs
 
 # TODO: Configure emacs daemon & client
 
-echo "DONE CONFIGURING EMACS!!"
+echo "Done configuring emacs!!"
+echo
