ec596a001b89522ed079ae543422d9c64b17acd5
tmt <minhtuan.tran96@gmail.com>
2019-01-20 02:20:10 -0800

    remove unnecessary cat

--- bash/install.sh
+++ bash/install.sh
@@ -8,18 +8,14 @@ echo "Configuring .bash_profile..."
 # commonrc
 test -f ~/.bash_profile || touch ~/.bash_profile
 if ! grep commonrc ~/.bash_profile > /dev/null 2>&1; then
-cat <<-EOF >> ~/.bash_profile
-source $dir_name/../.commonrc
-EOF
+    echo "source $dir_name/../.commonrc" >> ~/.bas_profile
 fi
 
 echo "Adding iTerm integration..."
 curl -L https://iterm2.com/shell_integration/bash \
 -o ~/.iterm2_shell_integration.bash
 if ! grep iterm2_shell_integration ~/.bash_profile > /dev/null 2>&1; then
-cat <<-EOF >> ~/.bash_profile
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
-EOF
+    echo "test -e \"${HOME}/.iterm2_shell_integration.bash\" && source \"${HOME}/.iterm2_shell_integration.bash\"" ~/.bash_profile
 fi
 
 source ~/.bash_profile
--- zsh/install.sh
+++ zsh/install.sh
@@ -15,9 +15,7 @@ echo "Configuring .zshenv"
 # commonrc
 test -f ~/.zshenv || touch ~/.zshenv
 if ! grep commonrc ~/.zshenv > /dev/null 2>&1; then
-cat <<-EOF >> ~/.zshenv
-source $dir_name/../.commonrc
-EOF
+    echo "source $dir_name/../.commonrc" >> ~/.zshenv
 fi
 
 # z-files
@@ -26,9 +24,7 @@ for file in "$dir_name/z*"; do
     echo "Configuring .${filename}"
     test -f ~/."$filename" || touch ~/."$filename"
     if ! grep "$file" ~/."$filename" > /dev/null 2>&1; then
-    cat <<-EOF >> ~/."$filename"
-source $file
-EOF
+        echo "source $file" >>  ~/."$filename"
     fi
 done
 
@@ -37,9 +33,7 @@ curl -L https://iterm2.com/shell_integration/zsh \
 -o ~/.iterm2_shell_integration.zsh
 test -f ~/.zshrc || touch ~/.zshrc
 if ! grep iterm2_shell_integration ~/.zshrc > /dev/null 2>&1; then
-cat <<-EOF >> ~/.zshrc
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-EOF
+    echo "test -e \"${HOME}/.iterm2_shell_integration.zsh\" && source \"${HOME}/.iterm2_shell_integration.zsh\"" ~/.zshrc
 fi
 
 echo "Done configuring zsh!!"
