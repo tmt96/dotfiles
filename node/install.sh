c5ba9a5611c3bec5f2bf7e6e35088d7406b3cdf8
Tuan Tran <tuanmt@fb.com>
2019-11-02 00:35:25 -0700

    update ruby & node layers

--- node/install.sh
+++ node/install.sh
@@ -7,10 +7,10 @@ echo
 echo "Install node using nodenv..."
 backup_and_link $dir_name/default-packages $(nodenv root)/default-packages
 
-LATEST_VERSION=$(nodenv install -l | egrep "^[[:space:]]*[0-9]+.[0-9]+.[0-9]+$"| tail -1)
+LATEST_VERSION=$(nodenv install -l | egrep "^[[:space:]]*[0-9]+.[0-9]+.[0-9]+$" | tail -1)
 LATEST_VERSION=${LATEST_VERSION// /}
-nodenv install $LATEST_VERSION -s
-nodenv global $LATEST_VERSION
+nodenv install $LATEST_VERSION -s > /dev/null 2>&1
+nodenv global $LATEST_VERSION > /dev/null 2>&1
 
 echo "Finished installing node!"
 echo
--- ruby/install.sh
+++ ruby/install.sh
@@ -9,15 +9,18 @@ backup_and_link $dir_name/default-gems $(rbenv root)/default-gems
 
 LATEST_VERSION=$(rbenv install -l | egrep "^[[:space:]]*[0-9]+.[0-9]+.[0-9]+$"| tail -1)
 LATEST_VERSION=${LATEST_VERSION// /}
-rbenv install $LATEST_VERSION -s
-rbenv global $LATEST_VERSION
+rbenv install $LATEST_VERSION -s > /dev/null 2>&1
+rbenv global $LATEST_VERSION > /dev/null 2>&1
 
 mkdir -p "$(rbenv root)"/plugins
 if [ ! -d "$(rbenv root)/plugins/rbenv-which-ext" ]; then
-    git clone https://github.com/yyuu/rbenv-which-ext.git "$(rbenv root)/plugins/rbenv-which-ext"
+    git clone https://github.com/yyuu/rbenv-which-ext.git "$(rbenv root)/plugins/rbenv-which-ext" > /dev/null 2>&1
 fi
 if [ ! -d "$(rbenv root)/plugins/rbenv-update" ]; then
-    git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
+    git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update" > /dev/null 2>&1
+fi
+if [ ! -d "$(rbenv root)/plugins/rbenv-gem-migrate" ]; then
+    git clone https://github.com/nabeo/rbenv-gem-migrate.git "$(rbenv root)/plugins/rbenv-gem-migrate" > /dev/null 2>&1
 fi
 
 echo "Finished installing ruby!"
